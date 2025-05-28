11ade4b683fc246380ac32225707a3097e530250
HKLADOI\anhtr <anhtrinhtran3@gmail.com>
2025-05-28 08:20:52 +0700

    fix local time

--- WifiCheckApp_API/Controllers/AdminController.cs
+++ WifiCheckApp_API/Controllers/AdminController.cs
@@ -19,9 +19,8 @@ namespace WifiCheckApp_API.Controllers
         }
         private DateTime GetDateTimeLocal(DateTime dateTime)
         {
-            var utcNow = DateTime.UtcNow;
             var vnTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
-            return TimeZoneInfo.ConvertTimeFromUtc(utcNow, vnTimeZone);
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime, vnTimeZone);
         }
 
         [HttpPut("{employeeId}")]
